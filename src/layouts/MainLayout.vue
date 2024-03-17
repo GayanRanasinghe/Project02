d90bdf3e9fa68bde3cd307511e80ef44d91f546c
gayanranasinghe <gayanmf@gmail.com>
2024-03-17 12:50:26 -0600

    About

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,36 +1,52 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-toolbar class="bg-primary text-white q-my-md shadow-2">
+  <div class="">
+    <q-toolbar class="bg-red text-white shadow-2"> <!-- Change background color to red -->
       <q-btn flat round dense icon="menu" class="q-mr-sm" />
       <q-separator dark vertical inset />
-      <q-btn stretch flat label="Link" />
 
       <q-space />
 
-      
       <q-separator dark vertical />
-      <q-btn stretch flat label="About" />
+      <q-btn stretch flat label="About" @click="navigateToAbout" /> <!-- Add click event to navigate to about page -->
       <q-separator dark vertical />
       <q-btn stretch flat label="Blog" />
     </q-toolbar>
-    
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
-</template>
-
-<script setup>
-import { ref } from 'vue'
-
-
-defineOptions({
-  name: 'MainLayout'
-})
-
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
-</script>
+  <div class="row" style="background: black;">
+    <div class="col-md-8 mainheader">
+      <h1>Welcome to KDJ Blog</h1>
+      <p>KDJ Blog is the place you can read eveyrhing in technology field.</p>
+    </div>
+    <div class="col-md-4">
+      <img src="https://i1.sndcdn.com/avatars-000602518917-kcph49-t500x500.jpg" style="width: 50%; padding: 0; margin: 0">
+    </div>
+  </div>
+  </div>
+  </template>
+  <script>
+  
+  </script>
+  <style>
+  h1{
+    color: white;
+    margin: 10px 0;
+    text-transform: uppercase;
+    font-weight: 600;
+  
+  }
+  
+  img{
+    width: 100%;
+    margin: auto;
+    padding: 0
+  }
+  
+  p{
+    margin: 0
+  }
+  .mainheader{
+    padding: 30px 60px;
+    color: white;
+    margin: 0;
+    margin: auto
+  }
+  </style>
