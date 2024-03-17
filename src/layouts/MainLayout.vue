3343990eeb64893f8d46d3e182d694f109ef6703
gayanranasinghe <gayanmf@gmail.com>
2024-03-17 10:35:59 -0600

    Add new section to header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,18 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <q-toolbar class="bg-primary text-white q-my-md shadow-2">
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+      <q-separator dark vertical inset />
+      <q-btn stretch flat label="Link" />
+
+      <q-space />
+
+      
+      <q-separator dark vertical />
+      <q-btn stretch flat label="About" />
+      <q-separator dark vertical />
+      <q-btn stretch flat label="Blog" />
+    </q-toolbar>
     
 
     <q-page-container>
